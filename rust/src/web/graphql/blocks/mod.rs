f534e3e7f188854eb12db9cd397869f700522006
Trevor Bernard <trevor@granola.team>
2024-04-26 19:11:55 -0400

    (gql) filter out coinbase rewards in feetransfer inside blocks gql
    This is a strange compatability hack with minaexplorer

--- rust/src/web/graphql/blocks/mod.rs
+++ rust/src/web/graphql/blocks/mod.rs
@@ -509,6 +509,7 @@ impl From<PrecomputedBlock> for Block {
         let fee_transfers: Vec<BlockFeetransfer> = InternalCommand::from_precomputed(&block)
             .into_iter()
             .map(|cmd| InternalCommandWithData::from_internal_cmd(cmd, &block))
+            .filter(|x| matches!(x, InternalCommandWithData::FeeTransfer { .. }))
             .map(|ft| ft.into())
             .collect();
 
